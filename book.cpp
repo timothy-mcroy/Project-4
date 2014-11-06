22714569c0a16d9d499418e29a98d6270301fb80
Timothy McRoy <timothy-mcroy@uiowa.edu>
2014-11-05 19:35:26 -0600

    Correcting the ostream friend function

--- book.cpp
+++ book.cpp
@@ -74,7 +74,7 @@ bool Book::operator==(const Book & b) const
 
 friend std::ostream & operator<<(std::ostream & os, const Book & b)
 {
-    os << "Title, Author, Year, ISBN, Rating = "<< m_title<<", "<<m_author<<", "<<m_year<<", "<<m_isbn<<", "<<m_rating;
+    os << "Title, Author, Year, ISBN, Rating = "<< b.m_title<<", "<<b.m_author<<", "<<b.m_year<<", "<<b.m_isbn<<", "<<b.m_rating;
     return os;
 }
 /*
